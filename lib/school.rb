d4f0d901a2f08cf09d0f19ca5db8cd89b786e096
github username <github email address>
2019-01-10 09:33:56 -0500

    School's out

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+class School
+
+  attr_accessor :school
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    if self.roster.include?(grade)
+      self.roster[grade].push (student)
+    else
+      self.roster[grade] = []
+      self.roster[grade].push(student)
+    end
+  end
+
+  def grade(grade)
+    self.roster[grade]
+  end
+
+  def sort
+    self.roster.each do |grade, students|
+      self.roster[grade] = students.sort
+    end
+  end
+
+end
